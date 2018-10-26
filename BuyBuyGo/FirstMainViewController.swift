abcb7be227b98770ce7c07b5a8c415726e37c014
liouyanting <msze5189@gmail.com>
2018-10-26 16:47:06 +0800

    First

---
+++ BuyBuyGo/FirstMainViewController.swift
@@ -0,0 +1,37 @@
+//
+//  FirstMainViewController.swift
+//  BuyBuyGo
+//
+//  Created by 劉彥廷 on 2018/10/25.
+//  Copyright © 2018年 liou. All rights reserved.
+//
+
+import UIKit
+
+class FirstMainViewController: DLHamburguerViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    override func awakeFromNib() {
+        self.contentViewController = self.storyboard?.instantiateViewController(withIdentifier: "DLDemoNavigationViewController")
+        
+        self.menuViewController = self.storyboard?.instantiateViewController(withIdentifier: "MenuTableViewController")
+    }
+    
+
+}
---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+  use_frameworks!
+
+  # Pods for BuyBuyGo
+  target 'BuyBuyGo' do
+
+  pod 'Firebase/Auth'
+  pod 'Firebase/Database'
+  pod 'Firebase/Core' 
+  pod 'GoogleSignIn'
+
+
+end
