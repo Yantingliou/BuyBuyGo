2ba8cb2a802a258cdd1eade270c7150214beda0c
liouyanting <msze5189@gmail.com>
2018-10-29 14:16:06 +0800

    新增 - 最殺特賣 & 現金回饋商店 ui

---
+++ BuyBuyGo/MenuTableview/BackCashStoreViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BackCashStoreViewController.swift
+//  BuyBuyGo
+//
+//  Created by 劉彥廷 on 2018/10/29.
+//  Copyright © 2018年 liou. All rights reserved.
+//
+
+import UIKit
+
+class BackCashStoreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
